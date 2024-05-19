87cbf4337690c7384ca9f0a11369cde6ff8d5e24
sourabhmandal <19mandal97@gmail.com>
2024-05-19 12:06:29 +0530

    add material UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "NextJS Template",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { Typography } from "@mui/joy";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Typography>Welcome, From material UI library</Typography>
+    <main>
+      <Typography level="h3">Welcome, From material UI library</Typography>
     </main>
   );
 }
